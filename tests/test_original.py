7bacd3165ab94ed0b1f17d67cd37a9082a44a0ad
J.P. Hutchins <jphutchins@gmail.com>
2023-01-29 02:12:15 -0800

    tests: setup coverage and test

--- tests/test_original.py
+++ tests/test_original.py
@@ -7,6 +7,14 @@ from pathlib import Path
 
 import pytest
 
+from zephyr2vsc.helpers import (
+    generate_compilation_db,
+    generate_vscode_config_jsons,
+    get_all_c_files_relative_path,
+    get_ninja_rules,
+    get_relevant_c_files_relative_path,
+)
+
 REPO_DIRECTORY = os.getcwd()
 ORIGINAL_SCRIPT_PATH = os.path.join(REPO_DIRECTORY, "tests", "original", "zephyr2vsc.py")
 assert os.path.isfile(ORIGINAL_SCRIPT_PATH)
@@ -46,6 +54,32 @@ def test_blinky(blinky: None):
 
     command = f"python -m zephyr2vsc {ZEPHYR_ARM_GCC_PATH} {ZEPHYR_PATH} {BUILD_PATH}"
     subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+    with open(os.path.join(BUILD_PATH, "zephyr_compile_db.json"), "r") as f:
+        compile_db = json.load(f)
+    with open(os.path.join(ZEPHYR_PATH, ".vscode", "settings.json"), "r") as f:
+        settings = json.load(f)
+    with open(os.path.join(ZEPHYR_PATH, ".vscode", "c_cpp_properties.json"), "r") as f:
+        c_properties = json.load(f)
+
+    assert compile_db_original == compile_db
+    assert set(settings_original["files.exclude"]) == set(settings["files.exclude"])
+    assert set(c_properties_original["configurations"][0]["browse"]["path"]) == set(
+        c_properties["configurations"][0]["browse"]["path"]
+    )
+
+    # test as module
+    build_path = os.path.abspath(BUILD_PATH)
+    src_path = os.path.abspath(ZEPHYR_PATH)
+
+    ninja_rules = get_ninja_rules(build_path)
+    used_c_files = get_relevant_c_files_relative_path(src_path, build_path)
+    all_c_files = get_all_c_files_relative_path(src_path)
+    unused_c_files = all_c_files - used_c_files
+    db_full_path = generate_compilation_db(build_path, ninja_rules)
+    generate_vscode_config_jsons(
+        unused_c_files, used_c_files, os.path.abspath(ZEPHYR_ARM_GCC_PATH), db_full_path, src_path
+    )
+
     with open(os.path.join(BUILD_PATH, "zephyr_compile_db.json"), "r") as f:
         compile_db = json.load(f)
     with open(os.path.join(ZEPHYR_PATH, ".vscode", "settings.json"), "r") as f:
